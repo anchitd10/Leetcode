566e51d9ef46063d1d4867ff5a689e5e8c612e98
Anchit Das <anchitd10@gmail.com>
2024-07-19 23:12:46 +0530

    Lucky Numbers in a matrix

--- Problem-1380.cpp
+++ Problem-1380.cpp
@@ -34,3 +34,29 @@ public:
         return ans;
     }
 };
+
+
+// Using two arrays for min and max
+class Solution {
+public:
+    vector<int> luckyNumbers (vector<vector<int>>& matrix) {
+        int n=matrix.size(), m=matrix[0].size();
+        vector<int> row(n, INT_MAX), col(m, INT_MIN), ans;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                row[i] = min(row[i], matrix[i][j]);
+                col[j] = max(col[j], matrix[i][j]);
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix[i][j] == row[i] && matrix[i][j] == col[j]){
+                    ans.push_back(matrix[i][j]);
+                }
+            }
+        }
+        return ans;
+    }
+};
