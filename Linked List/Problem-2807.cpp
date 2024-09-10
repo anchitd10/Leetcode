1e099e36aa2edf4691f7a78b6e3f994737c38cd3
Anchit Das <anchitd10@gmail.com>
2024-09-10 19:29:26 +0530

    Insert Greatest Common Divisors in Linked List

--- Linked List/Problem-2807.cpp
+++ Linked List/Problem-2807.cpp
@@ -47,3 +47,21 @@ public:
         return head;
     }
 };
+
+//recursive
+class Solution {
+public:
+    ListNode* insertGreatestCommonDivisors(ListNode* head) {
+        if(!head || !head->next){
+            return head;
+        }
+
+        ListNode* temp = insertGreatestCommonDivisors(head->next);
+        ListNode* gcdNode = new ListNode(__gcd(head->val, head->next->val));
+
+        gcdNode->next = temp;
+        head->next = gcdNode;
+
+        return head;
+    }
+};
