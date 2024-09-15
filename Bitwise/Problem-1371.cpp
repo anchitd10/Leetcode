1a02e7f786b0da62565aa001853d8434904a9cbf
Anchit Das <anchitd10@gmail.com>
2024-09-15 16:51:59 +0530

    Find the Longest Substring Containing Vowels in Even Counts

--- Bitwise/Problem-1371.cpp
+++ Bitwise/Problem-1371.cpp
@@ -49,3 +49,51 @@ public:
         return result;
     }
 };
+
+// 2nd approach
+use XOR operator instead of adding for every vowelState that way we dont need modulo
+if XOR for even count then automatically 0
+
+class Solution {
+public:
+    int findTheLongestSubstring(string s) {
+        unordered_map<string, int> mp;
+        vector<int> vowelState(5,0);
+        
+        string currState = "00000";
+        mp[currState] = -1;
+
+        int result = 0;
+
+        for(int i=0; i<s.length(); i++){
+            if(s[i] == 'a'){
+                vowelState[0] = (vowelState[0] ^ 1);
+            }
+            else if(s[i] == 'e'){
+                vowelState[1] = (vowelState[1] ^ 1);
+            }
+            else if(s[i] == 'i'){
+                vowelState[2] = (vowelState[2] ^ 1);
+            }
+            else if(s[i] == 'o'){
+                vowelState[3] = (vowelState[3] ^ 1);
+            }
+            else if(s[i] == 'u'){
+                vowelState[4] = (vowelState[4] + 1)%2;
+            }
+
+            currState = "";
+            for(int j=0; j<5; j++){
+                currState += to_string(vowelState[j]);
+            }
+
+            if(mp.find(currState) != mp.end()){
+                result = max(result, i - mp[currState]);
+            }
+            else{
+                mp[currState] = i;
+            }
+        }
+        return result;
+    }
+};
