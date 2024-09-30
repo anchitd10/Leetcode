963d63a8e7660601c3b338f655ffe46febf0e937
Anchit Das <anchitd10@gmail.com>
2024-09-30 22:10:52 +0530

    Design a Stack with Increment Operation

---
+++ Stack/Problem-1381.cpp
@@ -0,0 +1,61 @@
+// Problem-1381: Design a Stack with Increment Operation
+
+// Design a stack that supports increment operations on its elements.
+
+// Implement the CustomStack class:
+
+// CustomStack(int maxSize) Initializes the object with maxSize which is the maximum number of elements in the stack.
+// void push(int x) Adds x to the top of the stack if the stack has not reached the maxSize.
+// int pop() Pops and returns the top of the stack or -1 if the stack is empty.
+// void inc(int k, int val) Increments the bottom k elements of the stack by val. If there are less than k elements in the stack, increment all the elements in the stack.
+ 
+
+// Example 1:
+
+// Input
+// ["CustomStack","push","push","pop","push","push","push","increment","increment","pop","pop","pop","pop"]
+// [[3],[1],[2],[],[2],[3],[4],[5,100],[2,100],[],[],[],[]]
+// Output
+// [null,null,null,2,null,null,null,null,null,103,202,201,-1]
+
+class CustomStack {
+public:
+    vector<int> stack;
+    int maxSize;
+
+    //constructor
+    CustomStack(int maxSize) {
+        this->maxSize = maxSize;
+    }
+    
+    void push(int x) {
+        if (stack.size() < maxSize) {
+            stack.push_back(x);
+        }
+    }
+    
+    int pop() {
+        if (stack.empty()) {
+            return -1;
+        }
+        int topElement = stack.back();
+        stack.pop_back();
+        return topElement;
+    }
+    
+    void increment(int k, int val) {
+        //increment the first min(k, stack.size()) elements
+        int limit = min(k, (int)stack.size());
+        for (int i = 0; i < limit; ++i) {
+            stack[i] += val;
+        }
+    }
+};
+
+/**
+ * Your CustomStack object will be instantiated and called as such:
+ * CustomStack* obj = new CustomStack(maxSize);
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * obj->increment(k,val);
+ */
