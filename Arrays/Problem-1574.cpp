87a7b02f58dd6fe4a1d71f43235cf443b72490ab
Anchit Das <anchitd10@gmail.com>
2024-11-15 14:47:48 +0530

    Shortest Subarray to be Removed to Make Array Sorted

---
+++ Arrays/Problem-1574.cpp
@@ -0,0 +1,11 @@
+// Problem-1574: Shortest Subarray to be Removed to Make Array Sorted
+
+// Given an integer array arr, remove a subarray (can be empty) from arr such that the remaining elements in arr are non-decreasing.
+// Return the length of the shortest subarray to remove.
+// A subarray is a contiguous subsequence of the array.
+// Example 1:
+// Input: arr = [1,2,3,10,4,2,3,5]
+// Output: 3
+// Explanation: The shortest subarray we can remove is [10,4,2] of length 3. The remaining elements after that will be [1,2,3,3,5] which are sorted.
+// Another correct solution is to remove the subarray [3,10,4].
+
