fdaa191f592d4f15bbfa13af34fdb29a5aebd53f
Anchit Das <anchitd10@gmail.com>
2025-01-08 21:55:55 +0530

    Count Prefix and Suffix Pairs I

--- Strings/Problem-3042.cpp
+++ Strings/Problem-3042.cpp
@@ -36,3 +36,25 @@ public:
         return count;
     }
 };
+
+
+// using find()
+class Solution {
+public:
+    int countPrefixSuffixPairs(vector<string>& words) {
+        int n = words.size();
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                string str1 = words[i];
+                string str2 = words[j];
+
+                if(str2.find(str1) == 0 && str2.rfind(str1) == str2.length() - str1.length()){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
