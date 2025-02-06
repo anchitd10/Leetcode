4fbc8af7c1ca9e794bddc6ed00a8b8704910d77b
Anchit Das <anchitd10@gmail.com>
2025-02-06 21:06:19 +0530

    Tuple with Same Product

--- Hash Maps/Problem-1726.cpp
+++ Hash Maps/Problem-1726.cpp
@@ -44,3 +44,31 @@ public:
         return tuples * 8;
     }
 };
+
+
+// Optimised with Hash Map
+class Solution {
+public:
+    int tupleSameProduct(vector<int>& nums) {
+        int n = nums.size();
+        int tuples = 0;
+
+        unordered_map<int, int> mp;
+
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                int p = nums[i] * nums[j];
+                mp[p]++;
+            }
+        }
+
+        for(auto &it : mp){
+            int prod = it.first;
+            int freq = it.second;
+
+            tuples += (freq * (freq-1))/2;
+        }
+
+        return tuples * 8;
+    }
+};
