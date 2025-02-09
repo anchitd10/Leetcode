0acac302ec33d748fef246ff86a914ae5647c0c8
Anchit Das <anchitd10@gmail.com>
2025-02-10 02:18:25 +0530

    Count Number of Bad Pairs

---
+++ Hash Maps/Problem-2364.cpp
@@ -0,0 +1,30 @@
+// Problem-2364: Count Number of Bad Pairs
+
+// You are given a 0-indexed integer array nums. A pair of indices (i, j) is a bad pair if i < j and j - i != nums[j] - nums[i].
+// Return the total number of bad pairs in nums.
+// Example 1:
+// Input: nums = [4,1,3,3]
+// Output: 5
+// Explanation: The pair (0, 1) is a bad pair since 1 - 0 != 1 - 4.
+// The pair (0, 2) is a bad pair since 2 - 0 != 3 - 4, 2 != -1.
+// The pair (0, 3) is a bad pair since 3 - 0 != 3 - 4, 3 != -1.
+// The pair (1, 2) is a bad pair since 2 - 1 != 3 - 1, 1 != 2.
+// The pair (2, 3) is a bad pair since 3 - 2 != 3 - 3, 1 != 0.
+// There are a total of 5 bad pairs, so we return 5.
+
+class Solution {
+    public:
+        long long countBadPairs(vector<int>& nums) {
+            unordered_map<int, int> freq;
+            long good = 0;
+    
+            for (int i = 0; i < nums.size(); i++) {
+                int key = nums[i] - i;
+                good += freq[key]; // Get the count of good pairs for this key
+                freq[key]++; // Increment the count for this key
+            }
+    
+            long totalPairs = static_cast<long>(nums.size()) * (nums.size() - 1) / 2; // Total pairs (i, j) with i < j
+            return totalPairs - good;
+        }
+    };
