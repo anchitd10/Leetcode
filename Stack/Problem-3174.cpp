83d183a5879ce58b23f54dfbb3fdeead450c8933
Anchit Das <anchitd10@gmail.com>
2025-02-10 21:46:45 +0530

    Clear Digits

--- Stack/Problem-3174.cpp
+++ Stack/Problem-3174.cpp
@@ -44,3 +44,40 @@ public:
         return s;
     }
 };
+
+// Stack-Optimised
+
+class Solution
+{
+public:
+    string clearDigits(string s)
+    {
+        stack<char> st;
+
+        for (char &ch : s)
+        {
+            if (isdigit(ch))
+            {
+                if (!st.empty())
+                {
+                    st.pop();
+                }
+            }
+            else
+            {
+                st.push(ch);
+            }
+        }
+
+        string result = "";
+        while (!st.empty())
+        {
+            result.push_back(st.top());
+            st.pop();
+        }
+
+        reverse(begin(result), end(result));
+
+        return result;
+    }
+};
