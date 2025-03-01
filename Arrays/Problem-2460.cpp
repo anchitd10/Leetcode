478cb83d08ee7d9c4eff4d5ec4528b2744c94d79
Anchit Das <anchitd10@gmail.com>
2025-03-02 00:25:17 +0530

    Apply Operations to an Array

--- Arrays/Problem-2460.cpp
+++ Arrays/Problem-2460.cpp
@@ -47,3 +47,29 @@ public:
         return nums;
     }
 };
+
+
+// 1 pass
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        int n = nums.size();
+
+        int j=0;
+        for (int i = 0; i < n; i++) {
+            if (i + 1 < n && nums[i] == nums[i + 1] && nums[i] != 0) {
+                nums[i] *= 2;
+                nums[i + 1] = 0;
+            }
+
+            if (nums[i] != 0) {
+                if (i != j) {
+                    swap(nums[i], nums[j]);
+                }
+                j++;
+            }
+        }
+
+        return nums;
+    }
+};
