30255d4b7cb58d89c859a5055855d0a1fdeb64ab
Anchit Das <anchitd10@gmail.com>
2025-04-08 23:00:27 +0530

    Minimum Number of Operations to Make Elements in Array Distinct

--- Arrays/Problem-3396.cpp
+++ Arrays/Problem-3396.cpp
@@ -41,3 +41,24 @@ public:
         return count;
     }
 };
+// O(n) = n^2
+
+
+// Optimised - iterating from reverse and getting op. count
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        int n = nums.size();
+        unordered_set<int> freq;
+        int count = 0;
+
+        for (int i = n-1; i >= 0; i--) {
+            if (freq.count(nums[i])) {
+                return ceil((i+1)/3.0);
+            }
+            freq.insert(nums[i]);
+        }
+
+        return 0;
+    }
+};
