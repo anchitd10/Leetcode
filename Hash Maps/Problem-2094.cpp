5e037dd5171cce64527e55081501058a6fad746e
Anchit Das <anchitd10@gmail.com>
2025-05-13 00:53:07 +0530

    Finding 3-Digit Even Numbers

--- Hash Maps/Problem-2094.cpp
+++ Hash Maps/Problem-2094.cpp
@@ -44,3 +44,39 @@ public:
         return result;
     }
 };
+
+
+// Using Vector Map
+class Solution {
+public:
+    vector<int> findEvenNumbers(vector<int>& digits) {
+        vector<int> result;
+        vector<int> mp(10,0);
+
+        for(int &digit : digits){
+            mp[digit]++;
+        }
+
+        for(int i=1; i<=9; i++){
+            if(mp[i] == 0) continue;
+            mp[i]--;
+
+            for(int j=0; j<=9; j++){
+                if(mp[j] == 0) continue;
+                mp[j]--;
+
+                for(int k=0; k<=8; k += 2){
+                    if(mp[k] == 0) continue;
+                    mp[k]--;
+
+                    int num = i*100 + j*10 + k;
+                    result.push_back(num);
+                    mp[k]++;
+                }
+                mp[j]++;
+            }
+            mp[i]++;
+        }
+        return result;
+    }
+};
