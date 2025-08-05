fd67a9ba876110b158dea48fd59e7a369f2841cb
anchitd10 <anchitd10@gmail.com>
2025-08-05 21:25:05 +0530

    Fruit Into Baskets

---
+++ Sliding Window/Problem-904.cpp
@@ -0,0 +1,46 @@
+// Problem-904: Fruit Into Baskets
+
+// You are visiting a farm that has a single row of fruit trees arranged from left to right. The trees are represented by an integer array fruits where fruits[i] is the type of fruit the ith tree produces.
+// You want to collect as much fruit as possible. However, the owner has some strict rules that you must follow:
+// - You only have two baskets, and each basket can only hold a single type of fruit. There is no limit on the amount of fruit each basket can hold.
+// - Starting from any tree of your choice, you must pick exactly one fruit from every tree (including the start tree) while moving to the right. The picked fruits must fit in one of your baskets.
+// - Once you reach a tree with fruit that cannot fit in your baskets, you must stop.
+// Given the integer array fruits, return the maximum number of fruits you can pick.
+// Example:
+// Input: fruits = [1,2,3,2,2]
+// Output: 4
+// Explanation: We can pick from trees [2,3,2,2].
+// If we had started at the first tree, we would only pick from trees [1,2].
+
+
+// Concept: Sliding Window
+// Approach: Use a sliding window to keep track of the two types of fruits and their counts. Expand the window until we have more than two types, then contract it from the left.
+
+
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int n = fruits.size();
+        int i=0, j=0;
+        int count = 0;
+        unordered_map<int, int> mp;
+
+        while(j < n){
+            mp[fruits[j]]++;
+
+            if(mp.size() <= 2){
+                count = max(count, j - i + 1);
+            }
+            else{
+                mp[fruits[i]]--;
+                if(mp[fruits[i]] == 0){
+                    mp.erase(fruits[i]);
+                }
+                i++;
+            }
+            j++;
+        }
+
+        return count;
+    }
+};
