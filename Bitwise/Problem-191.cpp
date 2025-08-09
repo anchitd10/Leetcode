b509ed868dba42ce8d725db131d5d1fe34819700
anchitd10 <anchitd10@gmail.com>
2025-08-09 23:16:33 +0530

    Number of 1 Bits

--- Bitwise/Problem-191.cpp
+++ Bitwise/Problem-191.cpp
@@ -21,3 +21,18 @@ public:
         return count;
     }
 };
+
+
+// Approach 2:
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int count = 0;
+        while(n > 0){
+            count += (n % 2);
+            n /= 2;
+        }
+
+        return count;
+    }
+};
